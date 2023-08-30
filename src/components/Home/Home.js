5952c8bbba1cf56e3540618eb46f754401a6b93f
Esteban <costazul.dev@gmail.com>
2023-08-29 17:38:48 -0700

    Upgraded Home and NavBar!

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,33 @@
 import React from "react";
 
 import NavBar from "../PageTools/NavBar/NavBar";
+import headShot from "../../assets/images/Esteban.jpg";
 
 import styles from "./Home.module.css";
 
 const Home = () => {
-
-    return (
-      <div className={styles.Home}>
-        <NavBar />
-        <h1>Site under construction... ⚠️</h1>
+  return (
+    <div className={styles.Home}>
+      <NavBar />
+      <div className={styles.contents}>
+        <div className={styles.headShotContainer}>
+          <img src={headShot} alt="A Handsome Individual Named Esteban" />
+        </div>
+        <div className={styles.MainTitleContainer}>
+          <h1>Hola, Me Llamo Esteban!</h1>
+        </div>
+        <div className={styles.MoreTextContainer}>
+          <h2>I Believe In Doing Work That Matters.</h2>
+          <p>
+            My Projects Are About<br></br>Ease-Of-Use And Value
+          </p>
+        </div>
+        <div className={styles.HireMeContainer}>
+          <h3>Hire me.</h3>
+        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Home;
--- src/components/PageTools/NavBar/NavBar.js
+++ src/components/PageTools/NavBar/NavBar.js
@@ -1,55 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
+  const [isSmallScreen, setSmallScreen] = useState(window.innerWidth < 780);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSmallScreen(window.innerWidth < 780);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className={styles.nav}>
+    <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpacity : ""}`}>
       <div className={styles.container}>
-        <div className={styles.Home}>
-          <NavLink to="/home" className={styles.aboutMeLink}>
-            Home
-          </NavLink>
-        </div>
-        <div className={styles.links}>
-          <ul className={styles.nav_ul}>
-            <li>
-              <NavLink to="/home" className={styles.nav.a}>
-                About Me
+        {isSmallScreen ? (
+          <>
+            <div className={styles.burgerMenu} onClick={toggleMenu}>
+              ☰
+            </div>
+            {isMenuOpen && (
+              <div className={styles.dropdownMenu}>
+                <div className={styles.closeMenu} onClick={toggleMenu}>
+                  &times;
+                </div>
+                <ul className={styles.nav_ul}>
+                  <li>
+                    <NavLink to="/home" className={styles.nav.a}>
+                      About Me
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/experiences" className={styles.nav.a}>
+                      Experiences
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/projects" className={styles.nav.a}>
+                      Projects
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/projects" className={styles.nav.a}>
+                      Blog
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/projects" className={styles.nav.a}>
+                      Music
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/projects" className={styles.nav.a}>
+                      Closet-Cleanup
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/contact" className={styles.nav.a}>
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </>
+        ) : (
+          <>
+            <div className={styles.Home}>
+              <NavLink to="/" className={styles.aboutMeLink}>
+                Home
               </NavLink>
-            </li>
-            <li>
-              <NavLink to="/experiences" className={styles.nav.a}>
-                Experiences
+            </div>
+            <div className={styles.links}>
+              <ul className={styles.nav_ul}>
+                <li>
+                  <NavLink to="/about-me" className={styles.nav.a}>
+                    About Me
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/experiences" className={styles.nav.a}>
+                    Experiences
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/projects" className={styles.nav.a}>
+                    Projects
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/blog" className={styles.nav.a}>
+                    Blog
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/music" className={styles.nav.a}>
+                    Music
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/closet-cleanup" className={styles.nav.a}>
+                    Closet-Cleanup
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.contactMe}>
+              <NavLink to="/contact" className={styles.aboutMeLink}>
+                Contact
               </NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects" className={styles.nav.a}>
-                Projects
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects" className={styles.nav.a}>
-                Blog
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects" className={styles.nav.a}>
-                Music
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects" className={styles.nav.a}>
-                Closet-Cleanup
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.contactMe}>
-          <NavLink to="/contact" className={styles.aboutMeLink}>
-            Contact
-          </NavLink>
-        </div>
+            </div>
+          </>
+        )}
       </div>
     </nav>
   );

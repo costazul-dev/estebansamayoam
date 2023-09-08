181b2f0828c81effed83d0ef84fbfe14f6fc9656
Esteban <costazul.dev@gmail.com>
2023-09-08 16:00:52 -0700

    New edits to home

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -16,16 +16,18 @@ const Home = () => {
           <img src={headShot} alt="A Handsome Individual Named Esteban" />
         </div>
         <div className={styles.MainTitleContainer}>
-          <h1>Hola, Me Llamo Esteban!</h1>
+          <h1>Hi, My Name is Esteban!</h1>
         </div>
         <div className={styles.MoreTextContainer}>
-          <h2>I Believe In Doing Work That Matters.</h2>
+          <h2>Innovation, Service, Growth: The Principles of My Code.</h2>
           <p>
-            My Projects Are About<br></br>Ease-Of-Use And Value
+            I am a full-stack software engineer focused on product development
+            and solutions. Drawing from my values of curiosity, connection,
+            honesty, responsibility, and communication my mission is to
+            continuously grow and adapt in order to build software that not only
+            aligns with my interests, but also serves others and makes a
+            meaningful impact on the world.
           </p>
-        </div>
-        <div className={styles.HireMeContainer}>
-          <h3>Hire me</h3>
         </div>
       </div>
     </div>
@@ -33,3 +35,15 @@ const Home = () => {
 };
 
 export default Home;
+
+/* 
+
+Brand Statements: I use my technical expertise and passion for writing code to solve important customer 
+problems that help small business owners to create functional websites, servers and databases that exceed 
+customer expectations
+
+I leverage my knowledge of company products and solutions and expertise in online technology to facilitate
+customer inquires. The ability to solve problems for individuals comes naturally for me and helps me meet 
+customer expectations
+
+*/
